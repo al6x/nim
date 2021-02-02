a35924b7b1e3e37ea6d324b01e9dac0975584b7f
alexeypetrushin <alexey.petrushin@gmail.com>
2021-02-02 22:19:56 +0300

    Upd

--- setm.nim
+++ setm.nim
@@ -1,3 +1,7 @@
 import sets
 
 export sets
+
+# is_empty -----------------------------------------------------------------------------------------
+func is_empty*[T](s: HashSet[T]): bool {.inline.} = s.len == 0
+func is_blank*[T](s: HashSet[T]): bool {.inline.} = s.len == 0
