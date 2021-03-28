8cb817dfaa1b71a45d916dd66fb242482eb2e274
alexeypetrushin <alexey.petrushin@gmail.com>
2021-03-28 21:11:58 +0300

    Upd

--- web/fs_sessionsm.nim
+++ web/fs_sessionsm.nim
@@ -1,25 +1,38 @@
-import basem, logm, jsonm, fsm as fs, envm, persistencem
+import basem, logm, jsonm, fsm as fs, envm, persistencem, timem
 {.experimental: "code_reordering".}
 
 
 # FsSessions ---------------------------------------------------------------------------------------
 # Simple session with local file system as a storage
 type FsSessions*[T] = object
-  path*: string
+  path*:           string
+  expiration_sec*: int
 
 proc init*[T](
-  _:     type[FsSessions[T]],
-  path = env["fs_session_path", "./tmp/sessions"]
+  _:               type[FsSessions[T]],
+  path           = env["fs_session_path", "./tmp/sessions"],
+  expiration_sec = 30.days.seconds
 ): FsSessions[T] =
-  FsSessions[T](path: path)
+  FsSessions[T](path: path, expiration_sec: expiration_sec)
+
+
+# SessionData --------------------------------------------------------------------------------------
+type SessionData[T] = object
+  session:   T
+  timestamp: Time
+
+proc init[T](_: type[SessionData[T]], session: T): SessionData[T] =
+  SessionData[T](session: session, timestamp: Time.now)
 
 
 # [], []=, delete ----------------------------------------------------------------------------------
 proc `[]`*[T](sessions: FsSessions[T], id: string): T =
-  T.read_from(sessions.fs_path(id))
+  let data = SessionData[T].read_from(sessions.fs_path(id), () => SessionData.init(T.init))
+  if (Time.now.epoch - data.timestamp.epoch) < sessions.expiration_sec:
+    return data.session
 
 proc `[]=`*[T](sessions: FsSessions[T], id: string, session: T) =
-  session.write_to(sessions.fs_path(id))
+  SessionData.init(session).write_to(sessions.fs_path(id))
 
 proc delete*[T](sessions: FsSessions[T], id: string) =
   fs.delete(sessions.fs_path(id))
