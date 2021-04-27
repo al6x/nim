e095ba9dd5606b15c62c6ecd520922916308ec8c
alexeypetrushin <alexey.petrushin@gmail.com>
2021-04-27 14:01:12 +0300

    upd

--- nodem/nodem/net_nodem.nim
+++ nodem/nodem/net_nodem.nim
@@ -9,9 +9,9 @@ type NodeDefinition* = ref object
   timeout_ms*: int
 
 type Node* = ref object of RootObj
-  # Refer to nodes by node instead of urls, like `"red_node"` instead of `"tcp://localhost:4000"`
-  # Nodes usually not defined immediately, but at runtime config,
-  # immediately defined nodes are usually for temporarry.
+  # Refer to nodes by string id, instead of url, like `"red_node"` instead of `"tcp://localhost:4000"`
+  # Usually nodes defined not immediately, but later, at runtime config, like IoC.
+  # Immediately defined nodes are usually temporarry nodes.
   id*:  string
   def:  Option[NodeDefinition]
 
@@ -27,15 +27,8 @@ proc `==`*(a, b: Node): bool = a.id == b.id
 
 var nodes_definitions*: Table[Node, NodeDefinition]
 
-# proc is_defined*(node: Node): bool =
-#   node.def.is_some
-
 proc define*(node: Node, url: string, timeout_ms = default_timeout_ms): void =
   nodes_definitions[node] = NodeDefinition(url: url, timeout_ms: timeout_ms)
-
-# proc define*[N](node: N): N =
-#   if node.is_defined: node
-#   else:               N(id: node.id, def: node.definition.some)
 
 proc definition*(node: Node): NodeDefinition =
   if node.def.is_some:
@@ -50,12 +43,6 @@ proc definition*(node: Node): NodeDefinition =
     node.define fmt"tcp://localhost:{6000 + (node.id.hash.int mod 50000)}"
   nodes_definitions[node]
 
-
 proc `%`*(node: Node): JsonNode =
   # Always define node when converting to JSON
   %(id: node.id, def: node.definition.some)
-
-
-# proc node_from_json*[N](_: type[N], json: JsonNode): N =
-#   if json.kind == JString: N(id: json.get_str)
-#   else:                    json.to(N)
