b86ae55c4d0d1908198d3c8fec4c476d4b65618d
alexeypetrushin <alexey.petrushin@gmail.com>
2021-05-13 07:08:36 +0300

    upd

--- ws/pubsub.js
+++ ws/pubsub.js
@@ -1,40 +1,58 @@
-// First reconnect is instant, consequent reconnects are randomised progressive `+ increment_ms`
-class PubSub {
-  static last_messages_ids = {}
+(function () {
+  function info(msg) { console.log("  pubsub " + msg) }
+  function error(msg) { console.log("E pubsub " + msg) }
+  function warn(msg) { console.log("W pubsub " + msg) }
 
-  constructor(
-    url,
-    topics,
-    onmessage,
-    increment_ms           = 500,
-    max_reconnect_delay_ms = 5000
-  ) {
-    url = url + (url.includes("?") ? "&" : "?") + "topics=" + encodeURIComponent(topics.join(","))
-    console.log("pubsub connected to " + url)
-    this.url = url; this.onmessage = onmessage,
-    this.increment_ms = increment_ms, this.max_reconnect_delay_ms = max_reconnect_delay_ms
-    this._reconnect(1)
-  }
+  // First reconnect is instant, consequent reconnects are randomised progressive `+ increment_ms`
+  class PubSub {
+    static last_messages_ids = {}
 
-  close() {
-    if (!this.es) return
-    try { this.es.close() } catch (e) {}
-    this.es = null
-  }
+    constructor(
+      url,
+      topics,
+      onmessage,
+      increment_ms           = 500,
+      max_reconnect_delay_ms = 10000
+    ) {
+      url = url + (url.includes("?") ? "&" : "?") + "topics=" + encodeURIComponent(topics.join(","))
+      this.url = url; this.onmessage = onmessage,
+      this.increment_ms = increment_ms, this.max_reconnect_delay_ms = max_reconnect_delay_ms
+      this._reconnect(1)
+    }
 
-  _reconnect(attempt) {
-    this.close()
-    let es = this.es = new EventSource(this.url)
+    close() {
+      if (!this.es) return
+      // info("closing")
+      try { this.es.close() } catch (e) {}
+      this.es = null
+    }
 
-    setTimeout(() => {
-      let success = false
-      es.ononopen = (_event) => {
-        if (this.es != es) return
+    _reconnect(attempt) {
+      info("connecting to " + this.url)
+      let es = new EventSource(this.url)
+      this.es = es
+
+      function close_again() {
+        // Sometimes it's still not closed
+        warn("closing again")
+        try { es.close() } catch (e) {}
+      }
+
+      let success = false, closed = false
+      es.onopen = (_event) => {
+        if (closed) {
+          close_again()
+          return
+        }
+        info("connected")
         success = true
       }
 
       es.onmessage = (event) => {
-        if (this.es != es) return
+        if (closed) {
+          close_again()
+        }
+
         success = true
 
         let message = JSON.parse(event.data)
@@ -52,17 +70,33 @@ class PubSub {
       }
 
       es.onerror = (_event) => {
-        if (this.es != es) return
-        this._reconnect(success ? 1 : attempt + 1)
-      }
-    }, this._calculate_timeout_ms(attempt))
-  }
+        if (closed) {
+          close_again()
+          return
+        }
+        this.close()
+        closed = true
 
-  _calculate_timeout_ms(attempt) {
-    return attempt == 1 ?
-      0 :
-      // Randomising to distribute server load more evenly
-      Math.max(this.max_reconnect_delay_ms, (attempt - 1) * this.increment_ms * Math.random())
+        if (success) {
+          // First reconnect not counted as error
+          info("disconnected, reconnecting")
+          setTimeout(() => this._reconnect(1), 1)
+        } else {
+          let delay_ms = this._calculate_timeout_ms(success ? 1 : attempt + 1)
+          error("error, will try to reconnect for " + (attempt + 1) + "th time, after " + delay_ms + "ms")
+          setTimeout(() => this._reconnect(attempt + 1), delay_ms)
+        }
+      }
+    }
+
+    _calculate_timeout_ms(attempt) {
+      // Timeout is randomised, but it never will be more than the max timeout
+      let delay_ms = attempt == 1 ?
+        0 :
+        Math.min(this.max_reconnect_delay_ms, Math.pow(2, attempt - 1) * this.increment_ms)
+      // Randomising to distribute server load evenly
+      return Math.round(((Math.random() * delay_ms) + delay_ms) / 2)
+    }
   }
-}
-window.PubSub = PubSub
+  window.PubSub = PubSub
+})()
