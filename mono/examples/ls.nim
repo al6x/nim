acb349d17b126f99d71fcb28ac9c1016cfe9fed5
alexeypetrushin <alexey.petrushin@gmail.com>
2023-06-12 00:16:10 +1000

    upd

--- mono/examples/ls.nim
+++ mono/examples/ls.nim
@@ -6,13 +6,12 @@ type Ls = ref object of Component
 proc render*(self: Ls): El =
   el"":
     el("input", (autofocus: true, placeholder: "Path..."), it.bind_to(self.path))
-    let path = self.path
-    if not path.is_empty:
-      if fs.exist path:
+    if not self.path.is_empty:
+      if fs.exist self.path:
         for entry in fs.read_dir(self.path):
           el("", (text: entry.name))
       else:
-        el("", (text: fmt"Path '{path}' doesn't exist"))
+        el("", (text: fmt"Path '{self.path}' doesn't exist"))
 
 define_session LsSession, Ls
 run_http_server (url) => LsSession.init(Ls())
