ec1990e25ae2e833dd962918fdcddb7e9c840a6d
alexeypetrushin <alexey.petrushin@gmail.com>
2023-07-10 19:44:46 +1000

    upd

--- keep/bin/copy_notes.nim
+++ keep/bin/copy_notes.nim
@@ -1,5 +1,6 @@
 #!/usr/bin/env nim r
 import base, os
+let keep_dir = current_source_path().parent_dir.parent_dir
 
 let from_dir = "/alex/notes"
 let to_dir   = "/alex/projects/keep" # current_source_path().parent_dir.parent_dir.absolute_path
@@ -7,3 +8,6 @@ let files    = @["tutor.ft", "movies.ft", "movies", "everyday.ft", "sales.ft", "
 
 for name in files:
   fs.copy fmt"{from_dir}/{name}", fmt"{to_dir}/{name}"
+
+fs.copy fmt"{keep_dir}/examples/notes/sample.ft", fmt"{to_dir}/sample.ft"
+fs.copy fmt"{keep_dir}/examples/notes/sample",    fmt"{to_dir}/sample"
