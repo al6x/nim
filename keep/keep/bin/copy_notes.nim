8e2ad38e05336ffd2947dc2e1ff0c1fb6e080efe
alexeypetrushin <alexey.petrushin@gmail.com>
2023-07-11 17:39:12 +1000

    version 3.5.1 upd docs

--- keep/keep/bin/copy_notes.nim
+++ keep/keep/bin/copy_notes.nim
@@ -4,7 +4,7 @@ let keep_dir = current_source_path().parent_dir.parent_dir
 
 let from_dir = "/alex/notes"
 let to_dir   = "/alex/projects/keep" # current_source_path().parent_dir.parent_dir.absolute_path
-let files    = @["movies.ft", "movies", "everyday.ft", "sales.ft", "books.ft"]
+let files    = @["movies.ft", "movies", "sales.ft", "books.ft"]
 
 for name in files:
   fs.copy fmt"{from_dir}/{name}", fmt"{to_dir}/{name}"
